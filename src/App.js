0f66df50dc6e3dbd1ab993ddc67733dfe0a12639
AhmadSaman <ahmad.00079821@gmail.com>
2021-07-19 23:54:00 +0300

    lesson-3:Creating text field and handle input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Img from "./components/Img";
 import ImgGrid from "./components/ImgGrid";
+import LinkField from "./components/LinkField";
 function App() {
 	return (
 		<div className="App">
-			<Img />
-			<ImgGrid />
+			<LinkField />
 		</div>
 	);
 }
