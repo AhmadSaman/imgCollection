05dabed720196c6d2239f32eb87fb84612538530
AhmadSaman <ahmad.00079821@gmail.com>
2021-07-19 15:25:35 +0300

    Creating Components

---
+++ src/components/ImgGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImgGrid = () => {
+	return (
+		<div>
+			<h1 className="text-2xl text-center text-purple-500">
+				HI I'm Image Grid Component
+			</h1>
+		</div>
+	);
+};
+
+export default ImgGrid;
